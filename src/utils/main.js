df4c70b6d0fd55c5320a7064d277e10db3e09e2b
vovilonn <vovadesign0792@gmail.com>
2021-12-24 18:15:21 +0200

    change mobileNavbar swipe distance

--- src/utils/main.js
+++ src/utils/main.js
@@ -1,17 +1,1 @@
-export function burger() {
-    document.querySelector(".burger").addEventListener("click", function () {
-        const menu = document.querySelector(".menu"),
-            body = document.querySelector("body");
-
-        if (this.classList.contains("close")) {
-            this.classList.remove("close");
-            menu.classList.remove("visible");
-            // body.style.overflow = "initial";
-        } else {
-            window.scrollTo(0, 0);
-            this.classList.add("close");
-            menu.classList.add("visible");
-            body.style.overflow = "hidden";
-        }
-    });
-}
+export function burger() {}
